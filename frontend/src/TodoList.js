49978c9f3ae2769c733d349903204f675bd1107a
chang <totppcop@gmail.com>
2025-12-28 18:00:21 +0800

    k8s run normal

--- frontend/src/TodoList.js
+++ frontend/src/TodoList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://api.localhost/api/todos/";
+const API_URL = "http://localhost:8080/api/todos/";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
